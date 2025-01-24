28f637f9c41f535c700cd18d020b4f4ed8e8b14f
shloyem <39220943+Shloyem@users.noreply.github.com>
2025-01-24 21:37:34 +0200

    Add solution to Invert Binary Tree problem

---
+++ 2_226 Invert Binary Tree/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharpSolutions._2_226_Invert_Binary_Tree
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            DoInvert(root);
+            return root;
+        }
+
+        public void DoInvert(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            // if left child exists and isn't a leaf - invert
+            if (node.left != null && (node.left.left != null || node.left.right != null))
+                DoInvert(node.left);
+
+            // if right child exists and isn't a leaf - invert
+            if (node.right != null && (node.right.left != null || node.right.right != null))
+                DoInvert(node.right);
+
+            // if current node has:
+            // two childs - invert between them
+            // one child - child's side becomes null, and child moves to the other side
+            if (node.left != null)
+            {
+                if (node.right != null)
+                {
+                    TreeNode temp = node.left;
+                    node.left = node.right;
+                    node.right = temp;
+                }
+                else
+                {
+                    node.right = node.left;
+                    node.left = null;
+                }
+            }
+            else if (node.right != null)
+            {
+                node.left = node.right;
+                node.right = null;
+            }
+        }
+    }
+}
